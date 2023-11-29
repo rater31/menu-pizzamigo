1e42063c9bae1a6d867ec624bdad519e3b467a13
rater31 <85116780+rater31@users.noreply.github.com>
2023-11-29 12:35:52 -0300

    fede

---
+++ menu-pizzamigo/web/js/desayuno.js
@@ -0,0 +1,88 @@
+const tradicional = document.getElementById("tradicional");
+const continental = document.getElementById("continental");
+const licuados = document.getElementById("licuados");
+const light = document.getElementById("light");
+const proteico = document.getElementById("proteico");
+const jamonyqueso = document.getElementById("jamonyqueso");
+const americano = document.getElementById("americano");
+const comboAmericano = document.getElementById("comboAmericano");
+//infusiones
+const cafechico = document.getElementById("cafechico");
+const cafejarrito = document.getElementById("cafejarrito");
+const cafedoble = document.getElementById("cafedoble");
+const capuchino = document.getElementById("capuchino");
+const te = document.getElementById("te");
+const jugochico = document.getElementById("jugochico");
+const jugogrande = document.getElementById("jugogrande");
+const licuados2 = document.getElementById("licuados2");
+const submarino = document.getElementById("submarino");
+//cafeteria
+const medialuna = document.getElementById("medialuna");
+const criollo = document.getElementById("criollo");
+const medialunajyq = document.getElementById("medialunajyq");
+const tostadascampo = document.getElementById("tostadascampo");
+const manteca = document.getElementById("manteca");
+const huevo = document.getElementById("huevo");
+const palta = document.getElementById("palta");
+const jyq = document.getElementById("jyq");
+const mixjyq = document.getElementById("mixjyq");
+
+
+
+// desayunos
+
+tradicional.innerHTML="$1800"
+
+continental.innerHTML="$2000"
+
+licuados.innerHTML="$2100"
+
+light.innerHTML="$1700"
+
+proteico.innerHTML="$2700"
+
+jamonyqueso.innerHTML="$2200"
+
+americano.innerHTML="$2900"
+
+comboAmericano.innerHTML="$5000"
+
+//infusiones
+
+cafechico.innerHTML="$600"
+
+cafejarrito.innerHTML="$750"
+
+cafedoble.innerHTML="$900"
+
+capuchino.innerHTML="$1100"
+
+te.innerHTML="$500"
+
+jugochico.innerHTML="$900"
+
+jugogrande.innerHTML="$1500"
+
+licuados2.innerHTML="$1500"
+
+submarino.innerHTML="$1200"
+
+//cafeteria
+
+medialuna.innerHTML="$500"
+
+criollo.innerHTML="$300"
+
+madialunajyq.innerHTML="$800"
+
+tostadascampo.innerHTML="$300"
+
+manteca.innerHTML="$500"
+
+huevo.innerHTML="$1000"
+
+palta.innerHTML="$1000"
+
+jyq.innerHTML="$1000"
+
+mixjyq.innerHTML="$1500"
---
+++ menu-pizzamigo/web/js/pizza.js
@@ -0,0 +1,8 @@
+const pizzaMedio = document.getElementById("pizzaMedio");
+const pizzaMetro = document.getElementById("pizzaMetro");
+
+
+// pizzas
+pizzaMedio.innerHTML="$5000"
+pizzaMetro.innerHTML="$10000"
+
