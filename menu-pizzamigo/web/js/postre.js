567e44868c48bafbb6b772c169bdfa7931f9af2a
rater31 <fedehidalgo89@gmail.com>
2023-11-30 22:27:14 -0300

    fede

---
+++ menu-pizzamigo/web/js/postre.js
@@ -0,0 +1,21 @@
+const porcionesdetarta =document.getElementById("tarta");
+const flan = document.getElementById("flan");
+const budindepan = document.getElementById("budindepan");
+const helado = document.getElementById("helado");
+const ensaladadefrutas = document.getElementById("ensaladadefrutas");
+const shotdepostres = document.getElementById("shotdepostres");
+const qcayote = document.getElementById("qcayote");
+const batataalmibar = document.getElementById("batataalmibar");
+const coreanitoalmibar = document.getElementById("coreanitoalmibar");
+
+
+
+porcionesdetarta.innerHTML="$1100"
+flan.innerHTML="$1100"
+budindepan.innerHTML="$1100"
+helado.innerHTML="$1100"
+ensaladadefrutas.innerHTML="$1100"
+shotdepostres.innerHTML="$1100"
+qcayote.innerHTML="$1100"
+batataalmibar.innerHTML="$1100"
+coreanitoalmibar.innerHTML="$1100"
