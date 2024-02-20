01703f913b6280bbb9921299e14bbf5657da96a1
rater31 <85116780+rater31@users.noreply.github.com>
2024-02-20 12:45:25 -0300

    fede
    se agrego pizzeta al menú

--- menu-pizzamigo/web/js/pizza.js
+++ menu-pizzamigo/web/js/pizza.js
@@ -1,7 +1,9 @@
 const pizzaMedio = document.getElementById("pizzaMedio");
 const pizzaMetro = document.getElementById("pizzaMetro");
+const pizzeta = document.getElementById("pizzeta");
 
 
 // pizzas
 pizzaMedio.innerHTML="$8800"
 pizzaMetro.innerHTML="$17600"
+pizzeta.innerHTML="$5000"
